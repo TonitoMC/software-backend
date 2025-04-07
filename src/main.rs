47d567f6a3e8c0d6994aa2d2d86b19cc3df387b2
Jose Merida <joseameridac@gmail.com>
2025-04-06 22:44:16 -0600

    feat: add dependencies & implement basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // Build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // Run it with hyper on localhost:3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Server running on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
 }
